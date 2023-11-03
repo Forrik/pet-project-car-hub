ebf9a4ab4be671a1dd8613a0f75a045dfcab22c0
Forrik <ilya33533@mail.ru>
2023-11-03 08:50:04 +0500

    added searchBar

---
+++ components/CustomFilter.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import {CustomFilterProps} from "@/types/index";
+
+const CustomFilter = ({title}: CustomFilterProps) => {
+  return (
+    <div>{title}</div>
+  )
+}
+
+export default CustomFilter
---
+++ utils/index.ts
@@ -0,0 +1,45 @@
+import { CarProps, FilterProps } from '@/types';
+
+export async function fetchCars(filters: FilterProps) {
+    const {manufacturer, year, model, limit, fuel} = filters;
+    const headers = {
+            'X-RapidAPI-Key': '1e17434440msh909af6fb17d176dp11c98djsnb29ba737135e',
+            'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+    }
+
+    const response =  await fetch(  `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, {
+        headers: headers,
+    });
+    const result = await response.json();
+
+    return result
+}
+
+export const calculateCarRent = ( city_mpg: number, year: number) => {
+    const basePricePerDay = 50; // Base rental price per day in dollars
+    const mileageFactor = 0.1; // Additional rate per mile driven
+    const ageFactor = 0.05; // Additional rate per year of age
+
+    const mileageRate = city_mpg * mileageFactor;
+    const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+    const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+    return rentalRatePerDay.toFixed(0);
+}
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+    const url = new URL("https://cdn.imagin.studio/getimage");
+
+    const { make, model, year } = car;
+
+    url.searchParams.append("customer", 'hrjavascript-mastery');
+    if (angle) {
+        url.searchParams.append("make", make);
+        url.searchParams.append("modelFamily", model.split(' ')[0]);
+        url.searchParams.append("zoomType", "fullscreen");
+        url.searchParams.append("modelYear", `${year}`);
+        url.searchParams.append("angle", `${angle}`);
+    }
+    return `${url}`
+}
